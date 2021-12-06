90f2070ae99b7e3a6e087537574496c06b9c55bd
PauloHenriqueMagno <paulohm2309@gmail.com>
2021-12-06 13:30:20 -0300

    feat: update

--- app.js
+++ app.js
@@ -1,145 +1,82 @@
 const express = require('express');
-const uuid = require('uuid');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
-
 const app = express();
+const jsonParser = bodyParser.json();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(express.json());
 
+app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
+    next()
+});
+
 const products = {
-    comercial: [
-        {
-            id: 1,
-            position: 1,
-            name: "Filé de Frango Grelhado",
-            price: 18,
-            description: "Arroz, feijão, batata frita e salda de alface, tomate e cenoura",
-            img: ""
-        },
-    ],
-    porcoes: [
-        {
-            id: 1,
-            position: 1,
-            name: "Fritas",
-            price: 10,
-            img: ""
-        }
-    ],
-    bebidas: [
-        {
-            id: 1,
-            position: 1,
-            name: "Coca-Cola (lata)",
-            price: 5,
-            img: ""
-        }
-    ],
-    sobremesas: [
-        {
-            id: 1,
-            position: 1,
-            name: "Pudim Inteiro",
-            price: 20,
-            img: ""
-        }
-    ],
-    lanches: [
-        {
-            id: 1,
-            position: 1,
-            name: "X-Salada",
-            price: 15,
-            img: ""
-        },
-    ],
-    day: {
-        segunda: [
-            {
-                id: 1,
-                position: 1,
-                name: "Filé de Frango Grelhado",
-                price: 18,
-                description: "Arroz, feijão, batata frita e salda de alface, tomate e cenoura",
-                img: ""
-            },
-        ],
-        terca: [
-            {
-                id: 1,
-                position: 1,
-                name: "Filé de Frango Grelhado",
-                price: 18,
-                description: "Arroz, feijão, batata frita e salda de alface, tomate e cenoura",
-                img: ""
-            },
-        ],
-        quarta: [
-            {
-                id: 1,
-                position: 1,
-                name: "Filé de Frango Grelhado",
-                price: 18,
-                description: "Arroz, feijão, batata frita e salda de alface, tomate e cenoura",
-                img: ""
-            },
-        ],
-        quinta: [
-            {
-                id: 1,
-                position: 1,
-                name: "Filé de Frango Grelhado",
-                price: 18,
-                description: "Arroz, feijão, batata frita e salda de alface, tomate e cenoura",
-                img: ""
-            },
-        ],
-        sexta: [
-            {
-                id: 1,
-                position: 1,
-                name: "Filé de Frango Grelhado",
-                price: 18,
-                description: "Arroz, feijão, batata frita e salda de alface, tomate e cenoura",
-                img: ""
-            },
-        ],
-        sabado: [
-            {
-                id: 1,
-                position: 1,
-                name: "Filé de Frango Grelhado",
-                price: 18,
-                description: "Arroz, feijão, batata frita e salda de alface, tomate e cenoura",
-                img: ""
-            },
-        ],
+    comercial: [],
+    porcoes: [],
+    bebidas: [],
+    sobremesas: [],
+    lanches: [],
+    dias: {
+        segunda: [],
+        terca: [],
+        quarta: [],
+        quinta: [],
+        sexta: [],
+        sabado: [],
     },
 };
 
+const orderList = (list) => {
+    list = list.sort((a,b) => a.position - b.position);
+
+    for(let i = list.length; i > 0; i--){
+        list[i - 1].position = i;
+    }
+
+    return list
+}
+
 app.patch("/products/:type", (req, res) => {
-    res.send(200, products)
+    res.status(200).send(products)
 });
 
-app.post("/products/:type", (req, res) => {
+app.post("/products/:type", jsonParser, (req, res) => {
 
-    if(!!req.body.name && !!req.body.price){
+    if(!!req.body.name && !!req.body.price){  
         const list = products[req.params.type];
-        const lastProduct = list.length === 0 ? list[list.length - 1] : {id: 0, position: 0};
-
+        
+        const lastProduct = list.length !== 0 ? list[list.length - 1] : {id: 0, position: 0};
+        
         const newProduct = {
-            name: req.body.name,
-            price: req.body.price,
-            id: lastProduct.id + 1,
-            position: lastProduct.position + 1
+            "name": req.body.name,
+            "price": req.body.price,
+            "id": lastProduct.id + 1,
+            "position": lastProduct.position + 1
         };
         
         products[req.params.type].push(newProduct);
-
-        res.send(201, list);
+        
+        res.status(201).send(list);
     };
 
-    res.send(400, "Name and price are necessary, description is optional");
+    res.status(400).send("Name and price are necessary, description is optional");
+});
+
+app.delete("/products/:type", (req, res) => {
+    const id = req.body.id;
+    const list = products[req.params.type];
+
+    if(list.some(prod => prod.id == id)){
+        products[req.params.type] = list.filter(prod => prod.id !== id);
+        products[req.params.type] = orderList(products[req.params.type])
+        res.status(202).send(products[req.params.type]);
+    }
+    else{
+        res.status(400).send("Invalid ID");
+    };
 });
 
 app.get("/", (req, res) => {
